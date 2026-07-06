51754221f71b1c4b124ff89f4c6905dd527703e8
Karan <karansingh40p@gmail.com>
2026-07-06 19:35:06 +0530

    Time: 4 ms (33.2%), Space: 12.5 MB (13.24%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,43 +1,64 @@
 class Solution {
 public:
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,vector<int>&leftRow,vector<int>&upperDiag,vector<int>&lowerDiag,int n){
-            if(col==n){
-                ans.push_back(board);
-                return ;
-            }
+    // Function to check if placing a queen at (row, col) is safe
+    bool isSafe(int row, int col, vector<vector<char>> &board, int n) {
+        // Check all columns to the left in the same row
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q') return false;
+        }
 
-            for(int row=0;row<n;row++){
-                if(leftRow[row]==0 && lowerDiag[row+col]==0 &&
-                upperDiag[n-1 +col-row]==0){
-                    board[row][col]='Q';
-                    leftRow[row]=1;
-                    lowerDiag[row+col]=1;
-                    upperDiag[n-1+col-row]=1;
+        // Check upper-left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
 
-                    solve(col+1,board,ans,leftRow,upperDiag,lowerDiag,n);
+        // Check lower-left diagonal
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
 
-                    board[row][col]='.';
-                    leftRow[row]=0;
-                    lowerDiag[row+col]=0;
-                    upperDiag[n-1+col-row]=0;
-                }
-            }
+        // Return true if no attack is possible
+        return true;
     }
 
-
-
-
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<string>board(n);
-
-        string s(n,'.');
-        for(int i=0;i<n;i++){
-            board[i]=s;
+    // Backtracking function to place queens column by column
+    void solve(int col, vector<vector<char>> &board,
+               vector<vector<string>> &ans, int n) {
+        // If all columns are filled, add current board to answer
+        if (col == n) {
+            vector<string> temp;
+            for (int i = 0; i < n; i++) {
+                // Convert row vector to string
+                string row(board[i].begin(), board[i].end());
+                temp.push_back(row);
+            }
+            ans.push_back(temp);
+            return;
         }
-        vector<int>leftRow(n,0),upperDiag(2*n-1,0),lowerDiag(2*n-1,0);
-        solve(0,board,ans,leftRow,upperDiag,lowerDiag,n);
-        return ans;
 
+        // Try placing queen in all rows of current column
+        for (int row = 0; row < n; row++) {
+            // Place queen only if safe
+            if (isSafe(row, col, board, n)) {
+                // Place queen
+                board[row][col] = 'Q';
+                // Recurse for next column
+                solve(col + 1, board, ans, n); 
+                // Backtrack and remove queen
+                board[row][col] = '.';        
+            }
+        }
+    }
+
+    // Main function to call backtracking
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+
+        // Create empty board of size n x n
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+
+        // Start backtracking from column 0
+        solve(0, board, ans, n);
+        return ans;
     }
 };
